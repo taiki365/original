c7ce48271ec771eb6b66510f4ffb7accdb78e2f4
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-12-01 08:10:26 +0900

    投手成績機能実装

--- app/controllers/pitchers_controller.rb
+++ app/controllers/pitchers_controller.rb
@@ -1,20 +1,49 @@
 class PitchersController < ApplicationController
-  def index
-  end
+  before_action :set_pitcher, only: [:edit, :update, :show, :destroy]
 
+  def index
+    @pitcher = Pitcher.all
+  end
+  
   def new
+    @pitcher = Pitcher.new
   end
 
   def create
+    @pitcher = Pitcher.new(pitcher_params)
+    if @pitcher.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
+    if @pitcher.update(pitcher_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @pitcher.destroy
+    redirect_to action: :index
   end
 
+  private
+
+  def pitcher_params
+    params.require(:pitcher).permit(:pitcher_name, :damage_hit, :defense, :win, :lose, :conceded, :damage_homerun, :gave_dead_ball, :gave_walks, :inning, :damage_batting_average ,:damage_base_percentage, :qs, :damage_slugging_percentage)
+  end
+  
+  def set_pitcher
+    @pitcher = Pitcher.find(params[:id])
+  end
 end
--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -1,16 +1,16 @@
 class Pitcher < ApplicationRecord
   validates :pitcher_name,               presence: true
-  validates :damage_hit,                 presence: true
+  validates :damage_hit,                 presence: true,numericality: { greater_than: 0 }
   validates :defense,                    presence: true
-  validates :win,                        presence: true
-  validates :lose,                       presence: true
-  validates :conceded,                   presence: true
-  validates :damage_homerun,             presence: true
-  validates :gave_walks,                 presence: true
-  validates :gave_dead_ball,             presence: true
-  validates :inning,                     presence: true
+  validates :win,                        presence: true,numericality: { greater_than: 0 }
+  validates :lose,                       presence: true,numericality: { greater_than: 0 }
+  validates :conceded,                   presence: true,numericality: { greater_than: 0 }
+  validates :damage_homerun,             presence: true,numericality: { greater_than: 0 }
+  validates :gave_walks,                 presence: true,numericality: { greater_than: 0 }
+  validates :gave_dead_ball,             presence: true,numericality: { greater_than: 0 }
+  validates :inning,                     presence: true,numericality: { greater_than: 0 }
   validates :damage_batting_average,     presence: true
   validates :damage_base_percentage,     presence: true
-  validates :qs,                         presence: true
+  validates :qs,                         presence: true,numericality: { greater_than: 0 }
   validates :damage_slugging_percentage, presence: true
 end
