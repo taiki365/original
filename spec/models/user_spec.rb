319e847091d46bb720369d6388445556752a4da9
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-12-14 10:01:48 +0900

    userテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'メールアドレスが空では登録出来ない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'パスワードが空では登録出来ない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it 'メールアドレスが既に登録されている' do
+      @user.save
+      another_user = FactoryBot.create(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'パスワードが6文字以上ではない' do
+      user = FactoryBot.build(:user)
+      @user.password = 'ab345'
+      @user.password_confirmation = 'ab345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
+    end
+    it 'パスワードが確認と一致しない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it 'メールアドレスに@が含まれていない' do
+      @user.email = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Email is invalid'
+    end
+  end
 end
